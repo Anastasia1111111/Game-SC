5cc9cc901d74ccc6cee2bfd4d15af35358a5c4f6
Плакса Анастасия Олеговна <plaksa@sc.softclub.by>
2024-10-22 17:55:54 +0300

    Add files button, pop-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CellComponent } from './cell/cell.component';
 import { BoardComponent } from './board/board.component';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CellComponent, BoardComponent],
+  imports: [RouterOutlet, CellComponent, BoardComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
